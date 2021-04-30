7b310f6f6431840fa252ee0e926f8722f11f864c
ArkDreal <andrey.o.leal@gmail.com>
2021-04-30 15:01:17 -0300

    Exercício da Idade em horas e dias concluído

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IdadeCompleta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console . WriteLine ( " Qual é a sua idade? " );
+            int  idadePessoa  =  int . Parse ( Console . ReadLine ());
+
+            int  idadeMeses  =  idadePessoa  *  12 ;
+            int  idadeDias  =  idadePessoa  *  365 ;
+            int  idadeHoras  =  idadeDias  *  24 ;
+
+            Console . WriteLine ( " Sua idade em meses é: "  +  idadeMeses );
+            Console . WriteLine ( " Sua idade em dias é: "  +  idadeDias );
+            Console . WriteLine ( " Sua idade em horas é: "  +  idadeHoras );
+        }
+    }
+}
